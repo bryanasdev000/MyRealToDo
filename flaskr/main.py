1b39031b728ace09a512a1424813b34e817d0c26
Bryan A. S <bryanasdev000@gmail.com>
2020-05-31 06:00:12 -0300

    main.py: black pass

--- flaskr/main.py
+++ flaskr/main.py
@@ -9,58 +9,65 @@ from sqlite3 import connect
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return Response('MyRealToDo', status=200, mimetype='text/plain')
+    return Response("MyRealToDo", status=200, mimetype="text/plain")
 
 
-@app.route('/api/v1.0/tasks', methods=['GET'])
+@app.route("/api/v1.0/tasks", methods=["GET"])
 def getTasks():
     conn = connect("db")
     cur = conn.cursor()
-    cur.execute('SELECT * FROM todos')
-    r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+    cur.execute("SELECT * FROM todos")
+    r = [
+        dict((cur.description[i][0], value) for i, value in enumerate(row))
+        for row in cur.fetchall()
+    ]
     conn.close()
-    return jsonify(tasks = r)
+    return jsonify(tasks=r)
 
-    
-@app.route('/api/v1.0/tasks/<int:id>', methods=['GET'])
+
+@app.route("/api/v1.0/tasks/<int:id>", methods=["GET"])
 def getTask(id):
     conn = connect("db")
     cur = conn.cursor()
-    cur.execute('SELECT * FROM todos WHERE id=?', (id))
-    r = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+    cur.execute("SELECT * FROM todos WHERE id=?", (id))
+    r = [
+        dict((cur.description[i][0], value) for i, value in enumerate(row))
+        for row in cur.fetchall()
+    ]
     conn.close()
-    return jsonify(task = r)
+    return jsonify(task=r)
 
 
-@app.route('/api/v1.0/tasks', methods=['POST'])
-def addTasks():
+@app.route("/api/v1.0/tasks", methods=["POST"])
+def addTask():
     data = request.json
-    if 'nome' and 'prioridade' and 'status' and 'depende' in data:
+    if "nome" and "prioridade" and "status" and "depende" in data:
         conn = connect("db")
         cur = conn.cursor()
-        cur.execute('INSERT INTO todos (nome, prioridade, status, depende) VALUES (?,?,?,?)', (data['nome'],data['prioridade'],data['status'],data['depende']))
+        cur.execute(
+            "INSERT INTO todos (nome, prioridade, status, depende) VALUES (?,?,?,?)",
+            (data["nome"], data["prioridade"], data["status"], data["depende"]),
+        )
         conn.commit()
-        data['id'] = cur.lastrowid
+        data["id"] = cur.lastrowid
         conn.close()
         return jsonify(data)
     else:
-        return Response("Falta parametros!", status=400, mimetype='text/plain') 
+        return Response("Falta parametros!", status=400, mimetype="text/plain")
 
 
-@app.route('/api/v1.0/tasks/<int:id>', methods=['PUT'])
+@app.route("/api/v1.0/tasks/<int:id>", methods=["PUT"])
 def updateTask(id):
-    return 'updateTask()!'
+    return "updateTask()!"
 
 
-@app.route('/api/v1.0/tasks/<int:id>', methods=['DELETE'])
+@app.route("/api/v1.0/tasks/<int:id>", methods=["DELETE"])
 def deleteTask(id):
-    return 'deleteTask()!'
+    return "deleteTask()!"
 
 
-@app.route('/healtcheck', methods = ['GET'])
+@app.route("/healtcheck", methods=["GET"])
 def healthcheck():
-    return Response("Awake and Alive", status=200, mimetype='text/plain')
-
-
+    return Response("Awake and Alive", status=200, mimetype="text/plain")
